49ceda1c9bfbb55a46ab1570d749622e1af09cc8
Tatsushi Demachi <tdemachi@gmail.com>
2014-10-17 17:46:47 +0900

    Add values to be able to embed commit hash and build date in the command

--- pacicli.go
+++ pacicli.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"os"
+	"runtime"
 
 	"github.com/codegangsta/cli"
 	"github.com/tsukaeru/pacicli/command"
 )
 
+var (
+	commitHash string
+	buildDate  string
+)
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "pacicli"
-	app.Version = Version
+	app.Version = Version + "\nGo version: " + runtime.Version()
+	if len(commitHash) > 0 {
+		app.Version += "\nGit commit: " + commitHash
+	}
+	if len(buildDate) > 0 {
+		app.Version += "\nBuild date: " + buildDate
+	}
 	app.Usage = "Command line interface for Parallels Cloud Infrastructure"
 	app.Commands = command.Commands
 	app.EnableBashCompletion = true
